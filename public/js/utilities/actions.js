38053ba96f86cf4de1a21937ac3bfea83bfaa02d
MFauzanAP <muhammadfauzanaristyaputra@gmail.com>
2021-09-28 11:57:25 +0300

    Add color input utility action

--- public/js/utilities/actions.js
+++ public/js/utilities/actions.js
@@ -17,3 +17,34 @@ function shake(selector, delay = 500, callback = null) {
 	if (callback) setTimeout(callback(), delay);
 
 }
+
+//	Function called to color an input red
+function color_input(selector, duration = 5000, callback = null) {
+
+	//	Find element
+	var element = $(selector);
+
+	//	Exit function if element is not found
+	if (!element) return;
+
+	//	Change border color to red
+	element.css('border-color', 'red');
+
+	//	Subscrube an on click method to the element
+	element.on('click', () => {
+
+		//	Remove color
+		element.css('border-color', '');
+
+		//	Unsubscribe this event
+		element.off('click');
+
+	})
+
+	//	Remove color after duration
+	setTimeout(() => { element.css('border-color', '') }, duration);
+
+	//	Call callback after duration has elapsed
+	if (callback) setTimeout(callback(), duration);
+
+}
