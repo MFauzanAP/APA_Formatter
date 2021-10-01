c1c0e21dd20cf683c02cb416cbb11fd5f0ea92dd
MFauzanAP <muhammadfauzanaristyaputra@gmail.com>
2021-10-01 13:25:41 +0300

    Add full screen text editor

--- public/js/form/essay_input.js
+++ public/js/form/essay_input.js
@@ -1,9 +1,16 @@
 //	Get reference to essay editor toolbar elements
 const editor = $(`.essay_form .essay textarea`);
 const word_count = $(`.essay_form .essay .word_count`);
+const fullscreen_editor = $(`.modal .body textarea`);
+const fullscreen_word_count = $(`.modal .body .word_count`);
+const fullscreen_button = $(`.essay_form .essay .fullscreen_button`);
 
 //	Subscribe function to text area on edit
 editor.on('change keyup paste', handle_essay_text_change);
+fullscreen_editor.on('change keyup paste', handle_fullscreen_essay_text_change);
+
+//	Subscribe function to full screen button
+fullscreen_button.on('click', () => show_modal(`fullscreen editor confirm`, handle_editor_expand, handle_editor_collapse));
 
 //	Function called when essay is being edited
 function handle_essay_text_change () {
@@ -16,5 +23,37 @@ function handle_essay_text_change () {
 
 	//	Update word count
 	word_count.html(`${num_words} words`);
+
+}
+
+//	Function called when essay is being edited
+function handle_fullscreen_essay_text_change () {
+
+	//	Get a match of all non whitespace words
+	var matches = fullscreen_editor.val().match(/\S+/g)
+	
+	//	Calculate number of words
+	var num_words = matches ? matches.length : 0;
+
+	//	Update word count
+	fullscreen_word_count.html(`${num_words} words`);
+
+}
+
+//	Function called when the user expands the editor
+function handle_editor_expand () {
+
+	//	Set fullscreen editor text
+	fullscreen_editor.val(editor.val());
+	handle_fullscreen_essay_text_change();
+
+}
+
+//	Function called when the user collapses the editor
+function handle_editor_collapse () {
+
+	//	Set editor text
+	editor.val(fullscreen_editor.val());
+	handle_essay_text_change();
 
 }
--- public/js/utilities/modal.js
+++ public/js/utilities/modal.js
@@ -2,13 +2,16 @@
 var modal = $(`.modal`);
 
 //	Subscribe methods to modal elements
-$(`.modal`).on('click', (e) => { if (e.target !== e.currentTarget) return; hide_modal(); });
-$(`.modal .buttons .cancel`).on('click', hide_modal);
+$(`.modal`).on('click', (e) => { if (e.target !== e.currentTarget) return; hide_modal(e); });
+$(`.modal .buttons .cancel`).on('click', (e) => { close_callback = null; hide_modal(e); });
 $(`.modal .buttons .confirm`).on('click', hide_modal);
 $(`.modal .buttons .okay`).on('click', hide_modal);
 
+//	Variable to hold close callback function
+var close_callback = null;
+
 //	Function used to show modal
-function show_modal (type) {
+function show_modal (type, callback = null, c_callback = null) {
 
 	//	Fix body page
 	$(`body`).addClass('fixed');
@@ -19,16 +22,29 @@ function show_modal (type) {
 	//	Add classes from type
 	modal.addClass(type);
 
+	//	Set close callback
+	close_callback = c_callback;
+
+	//	Call callback function
+	if (callback) callback();
+
 }
 
 //	Function used to hide modal
-function hide_modal () {
+function hide_modal (e, callback = null) {
 
 	//	Reset body page
 	$(`body`).removeClass('fixed');
 
 	//	Close modal
 	modal.removeClass('active');
+
+	//	Call callback function
+	if (callback) callback();
+	if (close_callback) close_callback();
+
+	//	Reset close callback
+	close_callback = null;
 
 	//	Reset modal classes and hide after modal closes
 	setTimeout(() => modal[0].className = `modal`, 1000)
