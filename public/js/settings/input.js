7b77b2c17b23d22c45315e4e55bbdde333a5621b
MFauzanAP <muhammadfauzanaristyaputra@gmail.com>
2021-10-13 20:07:47 +0300

    Implement saving of settings values

---
+++ public/js/settings/input.js
@@ -0,0 +1,37 @@
+//	Subscribe function to all settings
+$(`.navbar .buttons .menu .settings_tab .row input[type="checkbox"]`).on('change', handle_checkbox_setting);
+$(`.navbar .buttons .menu .settings_tab .row input[type="number"]`).on('input', handle_numeric_setting);
+$(`.navbar .buttons .menu .settings_tab .row select`).on('change', handle_dropdown_setting);
+
+//	Function called when a checkbox setting is clicked
+function handle_checkbox_setting () {
+
+	//	Get checkbox state
+	var state = this.checked;
+
+	//	Update local storage
+	window.localStorage.setItem(this.id, state);
+
+}
+
+//	Function called when a numeric setting is changed
+function handle_numeric_setting () {
+
+	//	Get input value
+	var value = this.value;
+
+	//	Update local storage
+	window.localStorage.setItem(this.id, value);
+
+}
+
+//	Function called when a dropdown setting is changed
+function handle_dropdown_setting () {
+
+	//	Get input value
+	var value = this.value;
+
+	//	Update local storage
+	window.localStorage.setItem(this.id, value);
+
+}
