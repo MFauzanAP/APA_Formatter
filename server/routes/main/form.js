da5f003f5b5cde0da2ffb4f4f7eb2d1e220a76f9
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2021-10-28 08:36:56 +0300

    Add highlighting of current active page link

--- server/routes/main/form.js
+++ server/routes/main/form.js
@@ -12,9 +12,28 @@ const router = express.Router();
 router.get('/', (req, res) => {
 
 	//	Render form page
-	res.render('form', { layout: 'form' });
+	res.render('form', { 
+		layout		: 'form', 
+		page		: 'essay', 
+		is_active 
+	});
 
 })
+
+
+
+/* ===================================================== Helpers ==================================================== */
+
+//	Function used to check if this link should be active
+function is_active(page, index) {
+
+	//	Declare array of pages
+	var pages = [ 'submissions', 'essay', 'report' ];
+
+	//	Return active if this link is active
+	return pages.indexOf(page) == index ? 'active ' : '';
+
+}
 
 
 
--- server/routes/main/report.js
+++ server/routes/main/report.js
@@ -12,9 +12,28 @@ const router = express.Router();
 router.get('/', (req, res) => {
 
 	//	Render converter page
-	res.render('report', { layout: 'report' });
+	res.render('report', { 
+		layout		: 'report',
+		page		: 'report',
+		is_active
+	});
 
 })
+
+
+
+/* ===================================================== Helpers ==================================================== */
+
+//	Function used to check if this link should be active
+function is_active(page, index) {
+
+	//	Declare array of pages
+	var pages = [ 'submissions', 'essay', 'report' ];
+
+	//	Return active if this link is active
+	return pages.indexOf(page) == index ? 'active ' : '';
+
+}
 
 
 
